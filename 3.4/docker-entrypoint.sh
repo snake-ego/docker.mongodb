251fd014cbbe62e7be738a3cad536837b73a01cd
Igor Voropaev <snakework10@gmail.com>
2018-06-20 18:23:15 +0500

    [3.4] Updated enrypoint

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 set -e
 
+USER='mongodb'
+CMD='mongod'
+
+if [ -e '/etc/mongod.conf' ]; then CMD="$CMD -f /etc/mongod.conf"; fi
+
 if [ ! -d "/data/configdb" ]; then mkdir -p /data/configdb; fi
 if [ ! -d "/data/db" ]; then mkdir -p /data/db; fi
- 
-if [ "${1:0:1}" = '-' ]; then
-    set -- mongod "$@"
-fi
 
-# allow the container to be started with `--user`
-if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
-    [ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
-    [ "$(stat -c %U /data/configdb)" = mongodb ] || chown -R mongodb /data/configdb
-    exec "/usr/local/bin/suexec" mongodb "$@"
+if [ "${1:0:1}" = '-' ]; then 
+    set -- $CMD "$@"; 
+fi
+if [ "$1" = 'mongod' ] && [ "$(id -u)" = '0' ]; then
+    shift
+    
+    [ "$(stat -c %U /data/db)" = $USER ] || chown -R $USER /data/db
+    [ "$(stat -c %U /data/configdb)" = $USER ] || chown -R $USER /data/configdb
+    
+    exec "/usr/local/bin/suexec" $USER $CMD "$@"
 fi
 
 exec "$@"
